349c738094d389183eebd3d74ea84109f15b7045
habib-hb <developerhabib1230@gmail.com>
2024-05-02 11:27:27 +0600

    changes :)

--- my-store/app/components/nav/NavBar.tsx
+++ my-store/app/components/nav/NavBar.tsx
@@ -1,6 +1,29 @@
+import Link from "next/link";
+import Container from "../Container";
+
 const NavBar = () => {
     return ( 
-        <div>NavBar</div>
+        <div
+        className="sticky
+        top-0
+        bg-slate-200
+        w-full
+        shadow-sm
+        z-30"
+        >
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                    <div className="flex items-center justify-between gap-3 md:gap-0">
+                        <Link href="/">E-Shop</Link>
+                        <div className="hidden md:block">Search</div>
+                        <div className="flex items-center gap-8 md:gap-12">
+                            <div>CartCount</div>
+                            <div>UserMenu</div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+            </div>
      );
 }
  
--- my-store/app/page.tsx
+++ my-store/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <main>E-Commerce HB</main>
+    <div>E-Commerce HB</div>
   )
 }
