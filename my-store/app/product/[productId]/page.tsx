7e0b435cbd021fb37bb836df776af3e35fff879d
habib-hb <developerhabib1230@gmail.com>
2024-05-08 18:25:24 +0600

    awesome changes :)

--- my-store/app/product/[productId]/page.tsx
+++ my-store/app/product/[productId]/page.tsx
@@ -1,11 +1,20 @@
+import Container from "@/app/components/Container";
+import { product } from "@/utils/product";
+import ProductDetails from "./ProductDetails";
+
 interface IPrams {
     productId:string
 }
 
 const Product = ({params}:{params:IPrams}) => {
     console.log("params" , params)
+    product
     return ( 
-        <div>Product Page</div>
+        <div className="p-8">
+            <Container>
+                <ProductDetails product={product}/>
+            </Container>
+        </div>
      );
 }
  
