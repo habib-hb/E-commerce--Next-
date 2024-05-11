ba7e7308b2121a89640a13f6a0f362103bf22b70
habib-hb <developerhabib1230@gmail.com>
2024-05-11 18:39:49 +0600

    awesome awesome awesome :)

--- my-store/app/components/products/ProductImage.tsx
+++ my-store/app/components/products/ProductImage.tsx
@@ -39,9 +39,8 @@ const ProductImage:React.FC<ProductImageProps> = ({
         ">
      {product.images.map((image:SelectedImgType)=>{
         return (
-            <div key={image.color} onClick={()=> handleColorSelect(image)} className={
-                ``
-            } >
+            <div key={image.color} onClick={()=> handleColorSelect(image)} className={`relative w-[80%] aspect-square rounded border-teal-300 
+            ${cartProduct.selectedImg.color === image.color ? 'border-[1.5px]' : 'border-none' }`} >
                 <Image src={image.image} alt={image.color}
                 fill
                 className="object-contain"
@@ -50,7 +49,19 @@ const ProductImage:React.FC<ProductImageProps> = ({
         )
      })}
         </div>
-        <div></div>
+        <div className="col-span-5 relative aspect-square">
+         <Image fill 
+         src={cartProduct.selectedImg.image}
+         alt={cartProduct.name}
+         className="
+         w-full
+         h-full
+         object-contain
+         max-h-[500px]
+         min-h-[300px]
+         sm:min-h-[400px]
+         " />
+        </div>
 
     </div> );
 }
