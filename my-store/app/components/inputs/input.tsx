31c0b2a27e1c1a05366e0ee2e311e2510f548d5c
habib-hb <developerhabib1230@gmail.com>
2024-05-15 11:00:49 +0600

    changes :)

--- my-store/app/components/inputs/input.tsx
+++ my-store/app/components/inputs/input.tsx
@@ -37,25 +37,25 @@ const Input: React.FC<InputProps> = ({
         p-4
         pt-6
         outline-none
-        by-white
+        bg-white
         font-light
         border-2
         rounded-md
         transition
-        disabled: opacity-70
-        disabled: cursor-not-allowed
+        disabled:opacity-70
+        disabled:cursor-not-allowed
         ${errors[id] ? 'border-rose-400' : 'border-slate-300'}
         ${errors[id] ? 'focus:border-rose-400' : 'focus:border-slate-300'}
         `} />
 
          <label htmlFor={id}
-         className="
+         className={`
          absolute
          cursor-text
          text-md
          duration-150
          tranform
-          -translate-y-3
+         -translate-y-3
           top-5
           z-10
           origin-[0]
@@ -63,8 +63,9 @@ const Input: React.FC<InputProps> = ({
           peer-placeholder-shown:scale-100
           peer-placeholder-shown:translate-y-0
           peer-focus:scale-75
-          peer-focus:translate-y-4  
-         "
+          peer-focus:-translate-y-4  
+          ${errors[id] ? 'text-rose-500' : 'text-slate-400'}
+         `}
          >{label}</label>   
     </div>
     )
